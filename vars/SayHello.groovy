270656260a1dc305908dbcf98782a720dededd5d
Мироненко Сергей <ms@MacBook-Pro-Mironenko.local>
2020-07-27 15:35:35 +0300

    groovy lib add cluster4

--- vars/SayHello.groovy
+++ vars/SayHello.groovy
@@ -2,5 +2,6 @@ def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello, ${name}."
-    Cluster.view(this)
+    String tt=Cluster.view(this)
+    echo "Yo, ${tt}"
 }
